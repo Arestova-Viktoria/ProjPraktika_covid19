b270ea8067edf8b7bfe838b9d6f786e15618a404
Viktoria <vikaarestova01@gmail.com>
2021-07-13 14:32:02 +0700

    class PeopleList

---
+++ PeopleList.cpp
@@ -0,0 +1,1 @@
+#include "PeopleList.h"
---
+++ PeopleList.h
@@ -0,0 +1,9 @@
+#pragma once
+ref class PeopleList
+{
+private: //System::String^ filename = gcnew System::String("gamma.txt");
+public:
+	//void PeopleList::work_init(array<int^, 2>^ text_buttons);
+	//void PeopleList::work_create(array<int^, 2>^ text_buttons);
+};
+
