911e25012130cfe0ebb2bb60516d19eb4c22d5e6
Viktoria <vikaarestova01@gmail.com>
2021-07-13 12:23:49 +0700

    new buttons and matrix

--- FormData.h
+++ FormData.h
@@ -59,6 +59,10 @@ namespace ProjPraktika {
 	private: System::Windows::Forms::NumericUpDown^ numericUpDown9;
 	private: System::Windows::Forms::Label^ label_10;
 	private: System::Windows::Forms::NumericUpDown^ numericUpDown10;
+	private: System::Windows::Forms::Button^ button_restart;
+
+
+
 
 
 
@@ -106,6 +110,7 @@ namespace ProjPraktika {
 			this->numericUpDown9 = (gcnew System::Windows::Forms::NumericUpDown());
 			this->label_10 = (gcnew System::Windows::Forms::Label());
 			this->numericUpDown10 = (gcnew System::Windows::Forms::NumericUpDown());
+			this->button_restart = (gcnew System::Windows::Forms::Button());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->numericUpDown1))->BeginInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->numericUpDown2))->BeginInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->numericUpDown3))->BeginInit();
@@ -138,17 +143,17 @@ namespace ProjPraktika {
 				static_cast<System::Byte>(204)));
 			this->label1->Location = System::Drawing::Point(382, 9);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(11, 578);
+			this->label1->Size = System::Drawing::Size(11, 612);
 			this->label1->TabIndex = 1;
 			this->label1->Text = L"|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n"
-				L"|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|";
+				L"|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|";
 			// 
 			// button_lastExit
 			// 
 			this->button_lastExit->AutoSize = true;
 			this->button_lastExit->Font = (gcnew System::Drawing::Font(L"Broadway", 13.8F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->button_lastExit->Location = System::Drawing::Point(941, 550);
+			this->button_lastExit->Location = System::Drawing::Point(963, 584);
 			this->button_lastExit->Name = L"button_lastExit";
 			this->button_lastExit->Size = System::Drawing::Size(97, 37);
 			this->button_lastExit->TabIndex = 2;
@@ -173,7 +178,7 @@ namespace ProjPraktika {
 			this->button_back->AutoSize = true;
 			this->button_back->Font = (gcnew System::Drawing::Font(L"Broadway", 13.8F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->button_back->Location = System::Drawing::Point(22, 550);
+			this->button_back->Location = System::Drawing::Point(12, 584);
 			this->button_back->Name = L"button_back";
 			this->button_back->Size = System::Drawing::Size(91, 37);
 			this->button_back->TabIndex = 4;
@@ -185,7 +190,7 @@ namespace ProjPraktika {
 			// 
 			this->numericUpDown1->Font = (gcnew System::Drawing::Font(L"Calibri", 10.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->numericUpDown1->Location = System::Drawing::Point(271, 57);
+			this->numericUpDown1->Location = System::Drawing::Point(277, 57);
 			this->numericUpDown1->Maximum = System::Decimal(gcnew cli::array< System::Int32 >(4) { 20, 0, 0, 0 });
 			this->numericUpDown1->Name = L"numericUpDown1";
 			this->numericUpDown1->Size = System::Drawing::Size(92, 28);
@@ -197,7 +202,7 @@ namespace ProjPraktika {
 			this->button_enter->AutoSize = true;
 			this->button_enter->Font = (gcnew System::Drawing::Font(L"Broadway", 13.8F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->button_enter->Location = System::Drawing::Point(265, 550);
+			this->button_enter->Location = System::Drawing::Point(271, 584);
 			this->button_enter->Name = L"button_enter";
 			this->button_enter->Size = System::Drawing::Size(98, 37);
 			this->button_enter->TabIndex = 6;
@@ -221,7 +226,7 @@ namespace ProjPraktika {
 				static_cast<System::Byte>(204)));
 			this->label_2->Location = System::Drawing::Point(8, 94);
 			this->label_2->Name = L"label_2";
-			this->label_2->Size = System::Drawing::Size(255, 28);
+			this->label_2->Size = System::Drawing::Size(263, 28);
 			this->label_2->TabIndex = 8;
 			this->label_2->Text = L"Длительность симптомов:";
 			// 
@@ -229,7 +234,7 @@ namespace ProjPraktika {
 			// 
 			this->numericUpDown2->Font = (gcnew System::Drawing::Font(L"Calibri", 10.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->numericUpDown2->Location = System::Drawing::Point(271, 94);
+			this->numericUpDown2->Location = System::Drawing::Point(277, 94);
 			this->numericUpDown2->Maximum = System::Decimal(gcnew cli::array< System::Int32 >(4) { 20, 0, 0, 0 });
 			this->numericUpDown2->Name = L"numericUpDown2";
 			this->numericUpDown2->Size = System::Drawing::Size(92, 28);
@@ -250,7 +255,7 @@ namespace ProjPraktika {
 			// 
 			this->numericUpDown3->Font = (gcnew System::Drawing::Font(L"Calibri", 10.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->numericUpDown3->Location = System::Drawing::Point(271, 131);
+			this->numericUpDown3->Location = System::Drawing::Point(277, 131);
 			this->numericUpDown3->Maximum = System::Decimal(gcnew cli::array< System::Int32 >(4) { 20, 0, 0, 0 });
 			this->numericUpDown3->Name = L"numericUpDown3";
 			this->numericUpDown3->Size = System::Drawing::Size(92, 28);
@@ -271,7 +276,7 @@ namespace ProjPraktika {
 			// 
 			this->numericUpDown4->Font = (gcnew System::Drawing::Font(L"Calibri", 10.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->numericUpDown4->Location = System::Drawing::Point(271, 170);
+			this->numericUpDown4->Location = System::Drawing::Point(277, 170);
 			this->numericUpDown4->Name = L"numericUpDown4";
 			this->numericUpDown4->Size = System::Drawing::Size(92, 28);
 			this->numericUpDown4->TabIndex = 13;
@@ -283,15 +288,15 @@ namespace ProjPraktika {
 				static_cast<System::Byte>(204)));
 			this->label_5->Location = System::Drawing::Point(8, 209);
 			this->label_5->Name = L"label_5";
-			this->label_5->Size = System::Drawing::Size(255, 28);
+			this->label_5->Size = System::Drawing::Size(257, 51);
 			this->label_5->TabIndex = 14;
-			this->label_5->Text = L"Входная смертность (в %):";
+			this->label_5->Text = L"Входная смертность\r\n(в %):";
 			// 
 			// numericUpDown5
 			// 
 			this->numericUpDown5->Font = (gcnew System::Drawing::Font(L"Calibri", 10.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->numericUpDown5->Location = System::Drawing::Point(271, 209);
+			this->numericUpDown5->Location = System::Drawing::Point(277, 220);
 			this->numericUpDown5->Name = L"numericUpDown5";
 			this->numericUpDown5->Size = System::Drawing::Size(92, 28);
 			this->numericUpDown5->TabIndex = 15;
@@ -301,7 +306,7 @@ namespace ProjPraktika {
 			// 
 			this->label_6->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->label_6->Location = System::Drawing::Point(8, 246);
+			this->label_6->Location = System::Drawing::Point(8, 271);
 			this->label_6->Name = L"label_6";
 			this->label_6->Size = System::Drawing::Size(255, 28);
 			this->label_6->TabIndex = 16;
@@ -311,7 +316,7 @@ namespace ProjPraktika {
 			// 
 			this->numericUpDown6->Font = (gcnew System::Drawing::Font(L"Calibri", 10.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->numericUpDown6->Location = System::Drawing::Point(271, 246);
+			this->numericUpDown6->Location = System::Drawing::Point(277, 271);
 			this->numericUpDown6->Name = L"numericUpDown6";
 			this->numericUpDown6->Size = System::Drawing::Size(92, 28);
 			this->numericUpDown6->TabIndex = 17;
@@ -321,7 +326,7 @@ namespace ProjPraktika {
 			// 
 			this->label_7->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->label_7->Location = System::Drawing::Point(8, 283);
+			this->label_7->Location = System::Drawing::Point(8, 309);
 			this->label_7->Name = L"label_7";
 			this->label_7->Size = System::Drawing::Size(255, 28);
 			this->label_7->TabIndex = 18;
@@ -331,7 +336,7 @@ namespace ProjPraktika {
 			// 
 			this->numericUpDown7->Font = (gcnew System::Drawing::Font(L"Calibri", 10.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->numericUpDown7->Location = System::Drawing::Point(271, 283);
+			this->numericUpDown7->Location = System::Drawing::Point(277, 309);
 			this->numericUpDown7->Name = L"numericUpDown7";
 			this->numericUpDown7->Size = System::Drawing::Size(92, 28);
 			this->numericUpDown7->TabIndex = 19;
@@ -341,9 +346,9 @@ namespace ProjPraktika {
 			// 
 			this->label_8->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->label_8->Location = System::Drawing::Point(8, 323);
+			this->label_8->Location = System::Drawing::Point(8, 349);
 			this->label_8->Name = L"label_8";
-			this->label_8->Size = System::Drawing::Size(255, 54);
+			this->label_8->Size = System::Drawing::Size(263, 54);
 			this->label_8->TabIndex = 20;
 			this->label_8->Text = L"Эффективность прививки\r\nот данного штамма (в %):";
 			// 
@@ -351,7 +356,7 @@ namespace ProjPraktika {
 			// 
 			this->numericUpDown8->Font = (gcnew System::Drawing::Font(L"Calibri", 10.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->numericUpDown8->Location = System::Drawing::Point(271, 333);
+			this->numericUpDown8->Location = System::Drawing::Point(277, 365);
 			this->numericUpDown8->Name = L"numericUpDown8";
 			this->numericUpDown8->Size = System::Drawing::Size(92, 28);
 			this->numericUpDown8->TabIndex = 21;
@@ -361,17 +366,17 @@ namespace ProjPraktika {
 			// 
 			this->label_9->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->label_9->Location = System::Drawing::Point(8, 387);
+			this->label_9->Location = System::Drawing::Point(8, 416);
 			this->label_9->Name = L"label_9";
-			this->label_9->Size = System::Drawing::Size(267, 28);
+			this->label_9->Size = System::Drawing::Size(255, 55);
 			this->label_9->TabIndex = 22;
-			this->label_9->Text = L"Вместимость больниц (в %):";
+			this->label_9->Text = L"Вместимость больниц \r\n(в %):";
 			// 
 			// numericUpDown9
 			// 
 			this->numericUpDown9->Font = (gcnew System::Drawing::Font(L"Calibri", 10.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->numericUpDown9->Location = System::Drawing::Point(271, 387);
+			this->numericUpDown9->Location = System::Drawing::Point(277, 431);
 			this->numericUpDown9->Name = L"numericUpDown9";
 			this->numericUpDown9->Size = System::Drawing::Size(92, 28);
 			this->numericUpDown9->TabIndex = 23;
@@ -381,7 +386,7 @@ namespace ProjPraktika {
 			// 
 			this->label_10->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->label_10->Location = System::Drawing::Point(8, 428);
+			this->label_10->Location = System::Drawing::Point(8, 480);
 			this->label_10->Name = L"label_10";
 			this->label_10->Size = System::Drawing::Size(255, 52);
 			this->label_10->TabIndex = 24;
@@ -391,18 +396,32 @@ namespace ProjPraktika {
 			// 
 			this->numericUpDown10->Font = (gcnew System::Drawing::Font(L"Calibri", 10.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->numericUpDown10->Location = System::Drawing::Point(271, 440);
+			this->numericUpDown10->Location = System::Drawing::Point(277, 491);
 			this->numericUpDown10->Name = L"numericUpDown10";
 			this->numericUpDown10->Size = System::Drawing::Size(92, 28);
 			this->numericUpDown10->TabIndex = 25;
 			this->numericUpDown10->Value = System::Decimal(gcnew cli::array< System::Int32 >(4) { 10, 0, 0, 0 });
+			// 
+			// button_restart
+			// 
+			this->button_restart->AutoSize = true;
+			this->button_restart->Font = (gcnew System::Drawing::Font(L"Broadway", 13.8F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button_restart->Location = System::Drawing::Point(405, 584);
+			this->button_restart->Name = L"button_restart";
+			this->button_restart->Size = System::Drawing::Size(100, 37);
+			this->button_restart->TabIndex = 26;
+			this->button_restart->Text = L"Заново";
+			this->button_restart->UseVisualStyleBackColor = true;
+			this->button_restart->Click += gcnew System::EventHandler(this, &FormData::button_restart_Click);
 			// 
 			// FormData
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->BackColor = System::Drawing::Color::PapayaWhip;
-			this->ClientSize = System::Drawing::Size(1062, 606);
+			this->ClientSize = System::Drawing::Size(1072, 633);
+			this->Controls->Add(this->button_restart);
 			this->Controls->Add(this->numericUpDown10);
 			this->Controls->Add(this->label_10);
 			this->Controls->Add(this->numericUpDown9);
@@ -432,6 +451,7 @@ namespace ProjPraktika {
 			this->DoubleBuffered = true;
 			this->Name = L"FormData";
 			this->Text = L"Модель распространения коронавируса";
+			this->Load += gcnew System::EventHandler(this, &FormData::FormData_Load);
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->numericUpDown1))->EndInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->numericUpDown2))->EndInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->numericUpDown3))->EndInit();
@@ -446,6 +466,7 @@ namespace ProjPraktika {
 			this->PerformLayout();
 
 		}
+		array<Button^, 2>^ buttons;
 #pragma endregion
 
 
@@ -462,6 +483,39 @@ private: System::Void button_enter_Click(System::Object^ sender, System::EventAr
 	if (one == "20")
 		MessageBox::Show(this, "Всё ок", "Сообщение", MessageBoxButtons::OK, MessageBoxIcon::Information);
 
+}
+private: System::Void FormData_Load(System::Object^ sender, System::EventArgs^ e) {
+	
+	//отрисовка поля на форме (размер 21х21 кнопок)	
+	buttons = gcnew array<Button^, 2>(21, 21);
+
+	for (int i = 0; i < 21; i++)
+	{
+		for (int j = 0; j < 21; j++)
+		{
+			Button^ btn = gcnew Button();
+			btn->Size = Drawing::Size(20, 20);
+			btn->Location = Drawing::Point(330 + i * 20, 42 + j * 20);
+
+			btn->FlatStyle = FlatStyle::Flat;
+			btn->FlatAppearance->BorderColor = Color::LightGray;
+			Controls->Add(btn);
+			buttons[i, j] = btn;
+			if(i == 10 && j == 10)	//посередине первый зараженный, отмечен красным
+				buttons[i, j]->BackColor = Color::Red;
+		}
+	}
+}
+
+//перезапуск модели
+private: System::Void button_restart_Click(System::Object^ sender, System::EventArgs^ e) {
+	for (int j = 0; j < 21; j++) {
+		for (int i = 0; i < 21; i++) {
+			buttons[i, j]->FlatAppearance->BorderColor = Color::LightGray;
+			if (i == 10 && j == 10)	//посередине первый зараженный, отмечен красным
+				buttons[i, j]->BackColor = Color::Red;
+		}
+	}
 }
 };
 }
